b6e02171656d414be702f6030b526ef87a935ed2
Christoph Erkinger <christoph.erkinger@gmail.com>
2020-04-08 23:59:21 +0200

    Add cart-icon and cart-dropdown component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/shoppage/shoppage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -18,7 +18,7 @@ class App extends Component {
             if (userAuth) {
                 const currentUser = await createUserProfileDocument(userAuth);
                 currentUser.onSnapshot(snapshot => {
-                    setCurrentUser({currentUser: {id: snapshot.id, ...snapshot.data()}});
+                    setCurrentUser({id: snapshot.id, ...snapshot.data()});
 
                 });
             } else {
@@ -38,15 +38,20 @@ class App extends Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
-                    <Route path='/signin' component={SignInAndSignUpPage}/>
+                    <Route exact path='/signin'
+                           render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
                 </Switch>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
