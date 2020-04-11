fc8835d813c372d8f1068208bbeed3c675177a4a
Christoph Erkinger <christoph.erkinger@gmail.com>
2020-04-11 10:44:32 +0200

    Implement Add Cart Item action

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import './collection-item.styles.scss';
+import CustomButton from "../custom-button/custom-button.component";
+import {addItem} from "../../redux/cart/cart.actions";
+import {connect} from "react-redux";
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className="collection-item">
-        <div className="image" style={{backgroundImage: `url(${imageUrl})`}}/>
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
+const CollectionItem = ({item, addItem}) => {
+    return (
+        <div className="collection-item">
+            <div className="image" style={{backgroundImage: `url(${item.imageUrl})`}}/>
+            <div className="collection-footer">
+                <span className="name">{item.name}</span>
+                <span className="price">{item.price}</span>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted>Add to Cart</CustomButton>
         </div>
-    </div>
-)
+    );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,10 @@
+export const addItemToCart = (cartItems, itemToBeAdded) => {
+    const existingCartItem = cartItems.find(item => item.id === itemToBeAdded.id)
+    if (existingCartItem) {
+        return cartItems.map(cartItem => cartItem.id === itemToBeAdded.id ? {
+            ...cartItem,
+            quantity: cartItem.quantity + 1
+        } : cartItem);
+    }
+    return [...cartItems, {...itemToBeAdded, quantity: 1}];
+}
